fac0a3c0f7a339719686ad44c00974e80676c7e7
YuliiaSliepchenko <115469039+YuliiaSliepchenko@users.noreply.github.com>
2023-05-10 18:06:08 +0300

    api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='movies' element={<h1>Movies</h1>} />
+      <Route path='movies/:movieId' element={<h1>MovieDetails</h1>} />
+      <Route path='cast' element={<h1>Cast</h1>} />
+      <Route path='reviews' element={<h1>Reviews</h1>} />
+   </Routes>
   );
 };
---
+++ src/helpers/movieApi.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '907b63ed414d758da06a5e7732936b73';
+
+export async function getTranding() {
+    const res = await axios.get(
+        `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
+    );
+    return res.data;
+}
+export async function getSearchMovieByQuery(query) {
+    const res = await axios.get(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+    );
+    return res.data;
+}
+export async function getSearcMovieById(id) {
+    const res = await axios.get(
+        `${BASE_URL}/movie/${id}?api_key=${API_KEY}`
+    );
+    return res.data;
+}
+
+export async function getSearcCresitsById(id) {
+    const res = await axios.get(
+        `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}`
+    );
+    return res.data;
+}
+export async function getSearcReviewById(id) {
+    const res = await axios.get(
+        `${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}`
+    );
+    return res.data;
+}
